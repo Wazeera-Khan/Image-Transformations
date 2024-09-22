19b1bae886bdc21fbcf8b14dcb1c926a403be2f2
Wazeera Khan <wazeerakhan04@gmail.com>
2024-09-22 13:31:16 +0530

    rotates an image

--- alltransformations.py
+++ alltransformations.py
@@ -3,3 +3,9 @@ import cv2 as cv
 
 img = cv.imread('image.png')
 cv.imshow('original',img)
+
+rows, cols = img.shape[:2]
+
+M2 = cv.getRotationMatrix2D(((cols-1)/2.0,(rows-1)/2.0),90,1)
+dst2 = cv.warpAffine(img,M2,(cols,rows))
+cv.imshow('rotated', dst2)
