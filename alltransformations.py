b573af3eda0b133c0ec44e59c76a113e8f192fe7
Wazeera Khan <wazeerakhan04@gmail.com>
2024-09-22 17:00:59 +0530

    scaling, rotating & translating features added

--- alltransformations.py
+++ alltransformations.py
@@ -6,13 +6,16 @@ cv.imshow('original',img)
 
 rows, cols = img.shape[:2]
 
+#scaling an image
 res = cv.resize(img, None, fx=2, fy=2, interpolation=cv.INTER_CUBIC)
 cv.imshow('scaling',res)
 
+# rotating an image
 M2 = cv.getRotationMatrix2D(((cols-1)/2.0,(rows-1)/2.0),90,1)
 dst2 = cv.warpAffine(img,M2,(cols,rows))
 cv.imshow('rotated', dst2)
 
+# translating an image
 M = np.float32([[1, 0, 100], [0, 1, 50]])
 dst = cv.warpAffine(img, M, (cols, rows))
 cv.imshow('translate', dst)
