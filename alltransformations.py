631f06d68fe44c14986c5a7d9ebe07caf9e8214f
Wazeera Khan <wazeerakhan04@gmail.com>
2024-09-22 16:11:08 +0530

    scale image

--- alltransformations.py
+++ alltransformations.py
@@ -3,3 +3,8 @@ import cv2 as cv
 
 img = cv.imread('image.png')
 cv.imshow('original',img)
+
+rows, cols = img.shape[:2]
+
+res = cv.resize(img, None, fx=2, fy=2, interpolation=cv.INTER_CUBIC)
+cv.imshow('scaling',res)
