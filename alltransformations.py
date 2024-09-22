033ad8d183d25e46f413598c18ed446698ef7127
Wazeera Khan <wazeerakhan04@gmail.com>
2024-09-22 16:14:08 +0530

    translates an image

--- alltransformations.py
+++ alltransformations.py
@@ -3,3 +3,10 @@ import cv2 as cv
 
 img = cv.imread('image.png')
 cv.imshow('original',img)
+
+
+rows, cols = img.shape[:2]
+M = np.float32([[1, 0, 100], [0, 1, 50]])
+dst = cv.warpAffine(img, M, (cols, rows))
+
+cv.imshow('translate', dst)
